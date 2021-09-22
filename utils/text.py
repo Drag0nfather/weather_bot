50d5ef52f5251005783bda63ee2bd030357efba8
Andrew <Andrey18651@gmail.com>
2021-09-23 00:13:32 +0300

    add text file to create output text

---
+++ utils/text.py
@@ -0,0 +1,41 @@
+from utils.coordinates_stuff import get_coordinates_by_city, split_coordinates_to_dict
+from utils.entity import WeatherEntity
+from utils.weather import YandexWeatherConnector, WEATHER_API_KEY
+
+
+CONDITION_DICT = {
+    'clear': 'ясно',
+    'partly-cloudy': 'малооблачно',
+    'cloudy': 'облачно с прояснениями',
+    'overcast': 'пасмурно',
+    'drizzle': 'морось',
+    'light-rain': 'небольшой дождь',
+    'rain': 'дождь',
+    'moderate-rain': 'умеренно-сильный дождь',
+    'heavy-rain': 'сильный дождь',
+    'continuous-heavy-rain': 'длительный сильный дождь',
+    'showers': 'ливень',
+    'wet-snow': 'дождь со снегом',
+    'light-snow': 'небольшой снег',
+    'snow': 'снег',
+    'snow-showers': 'снегопад',
+    'hail': 'град',
+    'thunderstorm': 'гроза',
+    'thunderstorm-with-rain': 'дождь с грозой',
+    'thunderstorm-with-hail': 'гроза с градом',
+}
+
+
+def create_output_message(entity: WeatherEntity) -> str:
+    condition = entity.fact['condition']
+    feels_like = entity.fact['feels_like']
+    string = f'За окном {CONDITION_DICT[condition]}, ощущается как {feels_like}'
+    return string
+
+
+if __name__ == '__main__':
+    weather = YandexWeatherConnector(token=WEATHER_API_KEY)
+    coordinates = get_coordinates_by_city('Малаховка')
+    dict_coordinates = split_coordinates_to_dict(coordinates)
+    entity = weather.get_weather_by_cors(dict_coordinates)
+    print(create_output_message(entity))
